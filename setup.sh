a2193ad3682a4705b1f0465097bfd357de863ed6
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-01-13 14:47:59 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --ou
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 sudo apt-get update
 sudo apt-get install cloudflare-warp
+warp-cli register
+source runnable_loop.sh
